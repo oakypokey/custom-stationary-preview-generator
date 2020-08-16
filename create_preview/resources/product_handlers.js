c1d91df8d5e9dc2cb57f14b17f10f1304453feaa
Calvin <cod11@georgetown.edu>
2020-08-16 04:32:08 -0400

    asdfghjkl

--- create_preview/resources/product_handlers.js
+++ create_preview/resources/product_handlers.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
         // Get the photo from server
         $.ajax({
             type:"GET",
-            url: "https://the-paper-place-australia.myshopify.com/apps/generate-image?json="+encodeURIComponent(JSON.stringify(data_payload)),
+            url: "https://the-paper-place-australia.myshopify.com/apps/generate-image/generate-image?json="+encodeURIComponent(JSON.stringify(data_payload)),
             success: function(returned_data){
                 console.log(returned_data)
                 if($("#FeaturedMedia-pp-preview-pic").length){
