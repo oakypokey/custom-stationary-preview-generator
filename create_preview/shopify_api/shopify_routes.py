ac0e97a75558fe3192bb376ad52c1a8f79a7fc7f
Calvin <cod11@georgetown.edu>
2020-08-16 03:07:56 -0400

    fixing spelling mistake

--- create_preview/shopify_api/shopify_routes.py
+++ create_preview/shopify_api/shopify_routes.py
@@ -53,8 +53,8 @@ def add_script():
 
     if ACCESS_TOKEN:
         paper_place = ShopifyStoreClient(shop=shop, access_token=ACCESS_TOKEN)
-        paper_place.update_script_tag(id=2093, src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js")
-        paper_place.update_script_tag(id=8761, src="https://pp-custom-stationary-preview.herokuapp.com/static/product_handlers.js")
+        paper_place.create_script_tag(src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js")
+        paper_place.create_script_tag(src="https://pp-custom-stationary-preview.herokuapp.com/static/product_handlers.js")
         
     return redirect('/app_launched?hmac={hmac}&locale={locale}&session={session}&shop={shop}&timestamp={timestamp}'.format(hmac=hmac,locale=locale,session=session,shop=shop,timestamp=timestamp))
 
